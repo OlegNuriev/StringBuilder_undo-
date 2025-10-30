bbf36d303e932c918a1f76f44169b44fe7d4fddc
OlegNuriev <6956840@gmail.com>
2025-10-30 09:01:52 +0300

    Stream Parallel

--- src/main/java/tasks/Main.java
+++ src/main/java/tasks/Main.java
@@ -2,15 +2,25 @@ package tasks;
 
 import tasks.Collection.ClassFilter;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] rawData = {"oleg", "hello", "world"};
+        String[] rawData = {"Oleg", "hello", "world"};
 
         String[] result = ClassFilter.filter(rawData, String::toUpperCase);
 
         System.out.println(Arrays.toString(result));
 
+    };
+
+    public static <T> Map<T, Long> countElements(T[] array) {
+        return Arrays.stream(array)
+                .collect(Collectors.groupingBy(
+                        e -> e,
+                        Collectors.counting()
+                ));
     }
 }
